27e4f5b1f13103b3595aaffcbf678ccca62b5aa3
ich <icanhas@users.sf.net>
2012-01-28 17:57:21 +0000

    update standalone info

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -27,12 +27,12 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // A user mod should never modify this file
 
 #ifdef STANDALONE
-  #define PRODUCT_NAME			"iofoo3"
-  #define BASEGAME			"foobar"
-  #define CLIENT_WINDOW_TITLE     	"changeme"
-  #define CLIENT_WINDOW_MIN_TITLE 	"changeme2"
-  #define HOMEPATH_NAME_UNIX		".foo"
-  #define HOMEPATH_NAME_WIN		"FooBar"
+  #define PRODUCT_NAME			"yantar"
+  #define BASEGAME			"base"
+  #define CLIENT_WINDOW_TITLE     	"Yantar"
+  #define CLIENT_WINDOW_MIN_TITLE 	"Yantar"
+  #define HOMEPATH_NAME_UNIX		".yantar"
+  #define HOMEPATH_NAME_WIN		"yantar"
   #define HOMEPATH_NAME_MACOSX		HOMEPATH_NAME_WIN
   #define GAMENAME_FOR_MASTER		"foobar"	// must NOT contain whitespace
 //  #define LEGACY_PROTOCOL	// You probably don't need this for your standalone game
