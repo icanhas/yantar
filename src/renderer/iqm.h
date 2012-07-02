b13985f6b8a1373d69b46e038cd9f9698a6b7100
ich <icanhas@users.sf.net>
2012-07-02 17:43:35 +0100

    ref: cleanup iqm.h

--- src/renderer/iqm.h
+++ src/renderer/iqm.h
@@ -19,97 +19,102 @@
 #ifndef __IQM_H__
 #define __IQM_H__
 
-#define IQM_MAGIC	"INTERQUAKEMODEL"
-#define IQM_VERSION	2
-
-#define IQM_MAX_JOINTS	128
-
-typedef struct iqmheader {
-	char		magic[16];
-	unsigned int	version;
-	unsigned int	filesize;
-	unsigned int	flags;
-	unsigned int	num_text, ofs_text;
-	unsigned int	num_meshes, ofs_meshes;
-	unsigned int	num_vertexarrays, num_vertexes, ofs_vertexarrays;
-	unsigned int	num_triangles, ofs_triangles, ofs_adjacency;
-	unsigned int	num_joints, ofs_joints;
-	unsigned int	num_poses, ofs_poses;
-	unsigned int	num_anims, ofs_anims;
-	unsigned int	num_frames, num_framechannels, ofs_frames, ofs_bounds;
-	unsigned int	num_comment, ofs_comment;
-	unsigned int	num_extensions, ofs_extensions;
-} iqmHeader_t;
-
-typedef struct iqmmesh {
-	unsigned int	name;
-	unsigned int	material;
-	unsigned int	first_vertex, num_vertexes;
-	unsigned int	first_triangle, num_triangles;
-} iqmMesh_t;
+#define IQM_MAGIC "INTERQUAKEMODEL"
 
 enum {
-	IQM_POSITION = 0,
-	IQM_TEXCOORD = 1,
-	IQM_NORMAL = 2,
-	IQM_TANGENT = 3,
+	IQM_VERSION		= 2,
+	IQM_MAX_JOINTS	= 128,
+	
+	IQM_POSITION		= 0,
+	IQM_TEXCOORD	= 1,
+	IQM_NORMAL		= 2,
+	IQM_TANGENT		= 3,
 	IQM_BLENDINDEXES	= 4,
-	IQM_BLENDWEIGHTS	= 5,
+	IQM_BLENDWEIGHTS= 5,
 	IQM_COLOR		= 6,
-	IQM_CUSTOM		= 0x10
-};
-
-enum {
+	IQM_CUSTOM		= 0x10,
+	
 	IQM_BYTE	= 0,
-	IQM_UBYTE	= 1,
-	IQM_SHORT	= 2,
-	IQM_USHORT	= 3,
-	IQM_INT		= 4,
-	IQM_UINT	= 5,
-	IQM_HALF	= 6,
-	IQM_FLOAT	= 7,
-	IQM_DOUBLE	= 8,
+	IQM_UBYTE		= 1,
+	IQM_SHORT		= 2,
+	IQM_USHORT		= 3,
+	IQM_INT			= 4,
+	IQM_UINT			= 5,
+	IQM_HALF			= 6,
+	IQM_FLOAT		= 7,
+	IQM_DOUBLE		= 8,
+	
+	IQM_LOOP		= 1<<0
 };
 
-typedef struct iqmtriangle {
-	unsigned int vertex[3];
-} iqmTriangle_t;
+typedef struct iqmHeader_t iqmHeader_t;
+typedef struct iqmMesh_t iqmMesh_t;
+typedef struct iqmTriangle_t iqmTriangle_t;
+typedef struct iqmJoint_t iqmJoint_t;
+typedef struct iqmPose_t iqmPose_t;
+typedef struct iqmAnim_t iqmAnim_t;
+typedef struct iqmVertexArray_t iqmVertexArray_t;
+typedef struct iqmBounds_t iqmBounds_t;
 
-typedef struct iqmjoint {
-	unsigned int	name;
-	int		parent;
-	float		translate[3], rotate[4], scale[3];
-} iqmJoint_t;
-
-typedef struct iqmpose {
-	int		parent;
-	unsigned int	mask;
-	float		channeloffset[10];
-	float		channelscale[10];
-} iqmPose_t;
-
-typedef struct iqmanim {
-	unsigned int	name;
-	unsigned int	first_frame, num_frames;
-	float		framerate;
-	unsigned int	flags;
-} iqmAnim_t;
-
-enum {
-	IQM_LOOP = 1<<0
+struct iqmHeader_t {
+	char	magic[16];
+	uint	version;
+	uint	filesize;
+	uint	flags;
+	uint	num_text, ofs_text;
+	uint	num_meshes, ofs_meshes;
+	uint	num_vertexarrays, num_vertexes, ofs_vertexarrays;
+	uint	num_triangles, ofs_triangles, ofs_adjacency;
+	uint	num_joints, ofs_joints;
+	uint	num_poses, ofs_poses;
+	uint	num_anims, ofs_anims;
+	uint	num_frames, num_framechannels, ofs_frames, ofs_bounds;
+	uint	num_comment, ofs_comment;
+	uint	num_extensions, ofs_extensions;
 };
 
-typedef struct iqmvertexarray {
-	unsigned int	type;
-	unsigned int	flags;
-	unsigned int	format;
-	unsigned int	size;
-	unsigned int	offset;
-} iqmVertexArray_t;
+struct iqmMesh_t {
+	uint	name;
+	uint	material;
+	uint	first_vertex, num_vertexes;
+	uint	first_triangle, num_triangles;
+};
 
-typedef struct iqmbounds {
+struct iqmTriangle_t {
+	uint vertex[3];
+};
+
+struct iqmJoint_t {
+	uint	name;
+	int	parent;
+	float	translate[3], rotate[4], scale[3];
+};
+
+struct iqmPose_t {
+	int	parent;
+	uint	mask;
+	float	channeloffset[10];
+	float	channelscale[10];
+};
+
+struct iqmAnim_t {
+	uint	name;
+	uint	first_frame, num_frames;
+	float	framerate;
+	uint	flags;
+};
+
+struct iqmVertexArray_t {
+	uint	type;
+	uint	flags;
+	uint	format;
+	uint	size;
+	uint	offset;
+};
+
+struct iqmBounds_t {
 	float	bbmin[3], bbmax[3];
 	float	xyradius, radius;
-} iqmBounds_t;
+};
 
 #endif
