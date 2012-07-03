da89d4e54b34e5971c75b9fe3c5c2b1691610a03
ich <icanhas@users.sf.net>
2012-07-03 04:03:50 +0100

    sys: correct static specifiers in con_tty.c, cleanup

--- src/sys/con_tty.c
+++ src/sys/con_tty.c
@@ -1,4 +1,5 @@
-/*
+/* TTY routines */
+ /*
  * Copyright (C) 1999-2005 Id Software, Inc.
  *
  * This file is part of Quake III Arena source code.
@@ -29,49 +30,50 @@
 #include <sys/time.h>
 
 /*
- * tty console routines
- *
  * NOTE: if the user is editing a line when something gets printed to the early
- * console then it won't look good so we provide CON_Hide and CON_Show to be
+ * console then it won't look good so we provide Hide and Show to be
  * called before and after a stdout or stderr output
  */
+ 
+ enum { HISTLEN = 32 };
 
 extern qbool stdinIsATTY;
 static qbool stdin_active;
-/* general flag to tell about tty console mode */
-static qbool ttycon_on = qfalse;
+static qbool ttycon_on = qfalse;	/* general flag to tell about tty console mode */
 static int ttycon_hide = 0;
-
-/* some key codes that the terminal may be using, initialised on start up */
-static int	TTY_erase;
-static int	TTY_eof;
-
+static int	TTY_erase;	/* some key codes that the terminal */
+static int	TTY_eof;		/*   may be using; initialised on startup */
 static struct termios TTY_tc;
-
 static field_t TTY_con;
-
-/* This is somewhat of aduplicate of the graphical console history
- * but it's safer more modular to have our own here */
-#define CON_HISTORY 32
-static field_t ttyEditLines[ CON_HISTORY ];
-static int hist_current = -1, hist_count = 0;
+/* This is a bit of duplicate of the graphical console history
+ * but it's safer/more modular to have our own here */
+static field_t ttyEditLines[HISTLEN];
+static int hist_current = -1;
+static int hist_count = 0;
 
 /*
- * CON_FlushIn
- *
  * Flush stdin, I suspect some terminals are sending a LOT of shit
  * FIXME relevant?
  */
 static void
-CON_FlushIn(void)
+FlushIn(void)
 {
 	char key;
-	while(read(STDIN_FILENO, &key, 1)!=-1) ;
+	while(read(STDIN_FILENO, &key, 1) != -1)
+		;
 }
 
 /*
- * CON_Back
- *
+ * Reinitialize console input after receiving SIGCONT, as on Linux the terminal seems to lose all
+ * set attributes if user did CTRL+Z and then does fg again.
+ */
+static void
+SigCont(int signum)
+{
+	CON_Init();
+}
+
+/*
  * Output a backspace
  *
  * NOTE: it seems on some terminals just sending '\b' is not enough so instead we
@@ -79,7 +81,7 @@ CON_FlushIn(void)
  * (FIXME there may be a way to find out if '\b' alone would work though)
  */
 static void
-CON_Back(void)
+Back(void)
 {
 	char key;
 
@@ -92,13 +94,11 @@ CON_Back(void)
 }
 
 /*
- * CON_Hide
- *
  * Clear the display of the line currently edited
  * bring cursor back to beginning of line
  */
 static void
-CON_Hide(void)
+Hide(void)
 {
 	if(ttycon_on){
 		int i;
@@ -108,20 +108,18 @@ CON_Hide(void)
 		}
 		if(TTY_con.cursor>0)
 			for(i=0; i<TTY_con.cursor; i++)
-				CON_Back();
-		CON_Back();	/* Delete "]" */
+				Back();
+		Back();	/* Delete "]" */
 		ttycon_hide++;
 	}
 }
 
 /*
- * CON_Show
- *
  * Show the current line
  * FIXME need to position the cursor if needed?
  */
 static void
-CON_Show(void)
+Show(void)
 {
 	if(ttycon_on){
 		int i;
@@ -133,58 +131,33 @@ CON_Show(void)
 			if(TTY_con.cursor)
 				for(i=0; i<TTY_con.cursor; i++)
 					(void)write(STDOUT_FILENO,
-						TTY_con.buffer+i,
-						1);
+						TTY_con.buffer+i, 1);
 		}
 	}
 }
 
-/*
- * CON_Shutdown
- *
- * Never exit without calling this, or your terminal will be left in a pretty bad state
- */
-void
-CON_Shutdown(void)
-{
-	if(ttycon_on){
-		CON_Back();	/* Delete "]" */
-		tcsetattr (STDIN_FILENO, TCSADRAIN, &TTY_tc);
-	}
-
-	/* Restore blocking to stdin reads */
-	fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL,
-			0) & ~O_NONBLOCK);
-}
-
-/*
- * Hist_Add
- */
-void
-Hist_Add(field_t *field)
+static void
+HistAdd(field_t *field)
 {
 	int i;
-	assert(hist_count <= CON_HISTORY);
+	assert(hist_count <= HISTLEN);
 	assert(hist_count >= 0);
 	assert(hist_current >= -1);
 	assert(hist_current <= hist_count);
 	/* make some room */
-	for(i=CON_HISTORY-1; i>0; i--)
+	for(i=HISTLEN-1; i>0; i--)
 		ttyEditLines[i] = ttyEditLines[i-1];
 	ttyEditLines[0] = *field;
-	if(hist_count<CON_HISTORY)
+	if(hist_count<HISTLEN)
 		hist_count++;
 	hist_current = -1;	/* re-init */
 }
 
-/*
- * Hist_Prev
- */
-field_t *
-Hist_Prev(void)
+static field_t *
+HistPrev(void)
 {
 	int hist_prev;
-	assert(hist_count <= CON_HISTORY);
+	assert(hist_count <= HISTLEN);
 	assert(hist_count >= 0);
 	assert(hist_current >= -1);
 	assert(hist_current <= hist_count);
@@ -195,13 +168,10 @@ Hist_Prev(void)
 	return &(ttyEditLines[hist_current]);
 }
 
-/*
- * Hist_Next
- */
-field_t *
-Hist_Next(void)
+static field_t *
+HistNext(void)
 {
-	assert(hist_count <= CON_HISTORY);
+	assert(hist_count <= HISTLEN);
 	assert(hist_count >= 0);
 	assert(hist_current >= -1);
 	assert(hist_current <= hist_count);
@@ -212,23 +182,7 @@ Hist_Next(void)
 	return &(ttyEditLines[hist_current]);
 }
 
-/*
- * CON_SigCont
- * Reinitialize console input after receiving SIGCONT, as on Linux the terminal seems to lose all
- * set attributes if user did CTRL+Z and then does fg again.
- */
-
-void
-CON_SigCont(int signum)
-{
-	CON_Init();
-}
-
-/*
- * CON_Init
- *
- * Initialize the console input (tty mode if possible)
- */
+/* Initialize the console input (tty mode if possible) */
 void
 CON_Init(void)
 {
@@ -238,10 +192,8 @@ CON_Init(void)
 	 * then SIGTTIN or SIGTOU is emitted, if not caught, turns into a SIGSTP */
 	signal(SIGTTIN, SIG_IGN);
 	signal(SIGTTOU, SIG_IGN);
-
 	/* If SIGCONT is received, reinitialize console */
-	signal(SIGCONT, CON_SigCont);
-
+	signal(SIGCONT, SigCont);
 	/* Make stdin reads non-blocking */
 	fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL,
 			0) | O_NONBLOCK);
@@ -265,7 +217,7 @@ CON_Init(void)
 	 * characters  EOF,  EOL,  EOL2, ERASE, KILL, REPRINT,
 	 * STATUS, and WERASE, and buffers by lines.
 	 * ISIG: when any of the characters  INTR,  QUIT,  SUSP,  or
-	 * DSUSP are received, generate the corresponding sig­
+	 * DSUSP are received, generate the corresponding sigÂ€
 	 * nal
 	 */
 	tc.c_lflag &= ~(ECHO | ICANON);
@@ -281,9 +233,20 @@ CON_Init(void)
 	ttycon_on = qtrue;
 }
 
-/*
- * CON_Input
- */
+/* Never exit without calling this, or your terminal will be left in a pretty bad state */
+void
+CON_Shutdown(void)
+{
+	if(ttycon_on){
+		Back();	/* Delete "]" */
+		tcsetattr (STDIN_FILENO, TCSADRAIN, &TTY_tc);
+	}
+
+	/* Restore blocking to stdin reads */
+	fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL,
+			0) & ~O_NONBLOCK);
+}
+
 char *
 CON_Input(void)
 {
@@ -303,7 +266,7 @@ CON_Input(void)
 				if(TTY_con.cursor > 0){
 					TTY_con.cursor--;
 					TTY_con.buffer[TTY_con.cursor] = '\0';
-					CON_Back();
+					Back();
 				}
 				return NULL;
 			}
@@ -311,9 +274,8 @@ CON_Input(void)
 			if((key) && (key) < ' '){
 				if(key == '\n'){
 					/* push it in history */
-					Hist_Add(&TTY_con);
-					Q_strncpyz(text, TTY_con.buffer,
-						sizeof(text));
+					HistAdd(&TTY_con);
+					Q_strncpyz(text, TTY_con.buffer, sizeof(text));
 					Field_Clear(&TTY_con);
 					key = '\n';
 					(void)write(STDOUT_FILENO, &key, 1);
@@ -321,48 +283,37 @@ CON_Input(void)
 					return text;
 				}
 				if(key == '\t'){
-					CON_Hide();
+					Hide();
 					Field_AutoComplete(&TTY_con);
-					CON_Show();
+					Show();
 					return NULL;
 				}
 				avail = read(STDIN_FILENO, &key, 1);
-				if(avail != -1)
+				if(avail != -1){
 					/* VT 100 keys */
 					if(key == '[' || key == 'O'){
-						avail = read(STDIN_FILENO, &key,
-							1);
+						avail = read(STDIN_FILENO, &key, 1);
 						if(avail != -1){
 							switch(key){
 							case 'A':
-								history =
-									Hist_Prev();
+								history = HistPrev();
 								if(history){
-									CON_Hide();
-									TTY_con
-										=
-											*
-											history;
-									CON_Show();
+									Hide();
+									TTY_con = *history;
+									Show();
 								}
-								CON_FlushIn();
+								FlushIn();
 								return NULL;
 								break;
 							case 'B':
-								history =
-									Hist_Next();
-								CON_Hide();
+								history = HistNext();
+								Hide();
 								if(history)
-									TTY_con
-										=
-											*
-											history;
+									TTY_con = *history;
 								else
-									Field_Clear(
-										&
-										TTY_con);
-								CON_Show();
-								CON_FlushIn();
+									Field_Clear(&TTY_con);
+								Show();
+								FlushIn();
 								return NULL;
 								break;
 							case 'C':
@@ -372,10 +323,10 @@ CON_Input(void)
 							}
 						}
 					}
-				Q_DPrintf(
-					"droping ISCTL sequence: %d, TTY_erase: %d\n",
+				}
+				Q_DPrintf("dropping ISCTL sequence: %d, TTY_erase: %d\n",
 					key, TTY_erase);
-				CON_FlushIn();
+				FlushIn();
 				return NULL;
 			}
 			if(TTY_con.cursor >= sizeof(text) - 1)
@@ -386,7 +337,6 @@ CON_Input(void)
 			/* print the current line (this is differential) */
 			(void)write(STDOUT_FILENO, &key, 1);
 		}
-
 		return NULL;
 	}else if(stdin_active){
 		int len;
@@ -400,7 +350,6 @@ CON_Input(void)
 		if(select (STDIN_FILENO + 1, &fdset, NULL, NULL,
 			   &timeout) == -1 || !FD_ISSET(STDIN_FILENO, &fdset))
 			return NULL;
-
 		len = read(STDIN_FILENO, text, sizeof(text));
 		if(len == 0){	/* eof! */
 			stdin_active = qfalse;
@@ -410,24 +359,20 @@ CON_Input(void)
 		if(len < 1)
 			return NULL;
 		text[len-1] = 0;	/* rip off the /n and terminate */
-
 		return text;
 	}
 	return NULL;
 }
 
-/*
- * CON_Print
- */
 void
 CON_Print(const char *msg)
 {
-	CON_Hide( );
+	Hide();
 
 	if(com_ansiColor && com_ansiColor->integer)
 		Sys_AnsiColorPrint(msg);
 	else
 		fputs(msg, stderr);
 
-	CON_Show( );
+	Show();
 }
