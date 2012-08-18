0900978250ef332da0a6761cf8547f84e4a4b27c
ich <icanhas@users.sf.net>
2012-08-18 15:01:38 +0100

    common, paths: add models/players/

--- src/qcommon/q_paths.h
+++ src/qcommon/q_paths.h
@@ -1,10 +1,16 @@
+/* sound */
 #define Psound		"sound"
 #define Pmusic		Psound "/music"
 #define Pfeedback	Psound "/feedback"
+
 #define Pplayersounds	Psound "/player"
 #define Pannounce	Pplayersounds "/announce"
 
+/* vis (textures, models, etc.) */
 #define Pvis		"vis"
+
 #define Pgraphics	Pvis "/graphics"
 #define Pmenuart	Pgraphics "/menu"
-#define Pmodels	Pvis "/models"
+
+#define Pmodels		Pvis "/models"
+#define Pplayermodels	Pmodels "/players"
