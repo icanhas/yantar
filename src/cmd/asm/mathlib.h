34b02389dca760fe7aed4b5875d1ed302528a903
ich <icanhas@users.sf.net>
2012-09-15 08:26:40 +0100

    asm: fix missing types

--- src/cmd/asm/mathlib.h
+++ src/cmd/asm/mathlib.h
@@ -25,13 +25,13 @@
 #include <math.h>
 
 #ifdef DOUBLEVEC_T
-typedef double vec_t;
+typedef double Scalar;
 #else
-typedef float vec_t;
+typedef float Scalar;
 #endif
-typedef vec_t vec2_t[3];
-typedef vec_t vec3_t[3];
-typedef vec_t vec4_t[4];
+typedef Scalar Vec2[3];
+typedef Scalar Vec3[3];
+typedef Scalar Vec4[4];
 
 #define SIDE_FRONT	0
 #define SIDE_ON		2
@@ -42,7 +42,7 @@ typedef vec_t vec4_t[4];
 #define DEG2RAD(a)	(((a) * Q_PI) / 180.0F)
 #define RAD2DEG(a)	(((a) * 180.0f) / Q_PI)
 
-extern vec3_t vec3_origin;
+extern Vec3 vec3_origin;
 
 #define EQUAL_EPSILON 0.001
 
@@ -53,7 +53,7 @@ extern vec3_t vec3_origin;
 #define PLANE_Z		2
 #define PLANE_NON_AXIAL 3
 
-qbool VectorCompare(const vec3_t v1, const vec3_t v2);
+qbool VectorCompare(const Vec3 v1, const Vec3 v2);
 
 #define Vec3Dot(x,y)		(x[0]*y[0]+x[1]*y[1]+x[2]*y[2])
 #define Vec3Sub(a,b,c)	{c[0]=a[0]-b[0]; c[1]=a[1]-b[1]; c[2]=a[2]-b[2]; }
@@ -62,32 +62,32 @@ qbool VectorCompare(const vec3_t v1, const vec3_t v2);
 #define VectorScale(a,b,c)	{c[0]=b*a[0]; c[1]=b*a[1]; c[2]=b*a[2]; }
 #define VectorClear(x)		{x[0] = x[1] = x[2] = 0; }
 #define VectorNegate(x)		{x[0]=-x[0]; x[1]=-x[1]; x[2]=-x[2]; }
-void Vec10Copy(vec_t *in, vec_t *out);
+void Vec10Copy(Scalar *in, Scalar *out);
 
-vec_t Q_rint(vec_t in);
-vec_t _Vec3Dot(vec3_t v1, vec3_t v2);
-void _Vec3Sub(vec3_t va, vec3_t vb, vec3_t out);
-void _Vec3Add(vec3_t va, vec3_t vb, vec3_t out);
-void _Vec3Copy(vec3_t in, vec3_t out);
-void _VectorScale(vec3_t v, vec_t scale, vec3_t out);
+Scalar Q_rint(Scalar in);
+Scalar _Vec3Dot(Vec3 v1, Vec3 v2);
+void _Vec3Sub(Vec3 va, Vec3 vb, Vec3 out);
+void _Vec3Add(Vec3 va, Vec3 vb, Vec3 out);
+void _Vec3Copy(Vec3 in, Vec3 out);
+void _VectorScale(Vec3 v, Scalar scale, Vec3 out);
 
-double Vec3Len(const vec3_t v);
+double Vec3Len(const Vec3 v);
 
-void Vec3MA(const vec3_t va, double scale, const vec3_t vb, vec3_t vc);
+void Vec3MA(const Vec3 va, double scale, const Vec3 vb, Vec3 vc);
 
-void Vec3Cross(const vec3_t v1, const vec3_t v2, vec3_t cross);
-vec_t Vec3Normalize(const vec3_t in, vec3_t out);
-vec_t ColorNormalize(const vec3_t in, vec3_t out);
-void Vec3Inverse(vec3_t v);
+void Vec3Cross(const Vec3 v1, const Vec3 v2, Vec3 cross);
+Scalar Vec3Normalize(const Vec3 in, Vec3 out);
+Scalar ColorNormalize(const Vec3 in, Vec3 out);
+void Vec3Inverse(Vec3 v);
 
-void ClearBounds(vec3_t mins, vec3_t maxs);
-void AddPointToBounds(const vec3_t v, vec3_t mins, vec3_t maxs);
+void ClearBounds(Vec3 mins, Vec3 maxs);
+void AddPointToBounds(const Vec3 v, Vec3 mins, Vec3 maxs);
 
-qbool PlaneFromPoints(vec4_t plane, const vec3_t a, const vec3_t b,
-			 const vec3_t c);
+qbool PlaneFromPoints(Vec4 plane, const Vec3 a, const Vec3 b,
+			 const Vec3 c);
 
-void NormalToLatLong(const vec3_t normal, byte bytes[2]);
+void NormalToLatLong(const Vec3 normal, byte bytes[2]);
 
-int     PlaneTypeForNormal(vec3_t normal);
+int     PlaneTypeForNormal(Vec3 normal);
 
 #endif
