629ab80f0c8f86c428d75cd6a896eb8b4ffeeb49
ich <icanhas@users.sf.net>
2012-12-06 01:10:14 +0000

    getlibs: look for prefixes

--- cross-getlibs-mingw.sh
+++ cross-getlibs-mingw.sh
@@ -6,11 +6,12 @@
 # Also, you need to run this with root privileges.
 
 chain=
+prefix=
 procs=4
 # amd64 chains are given precedence here
 prefixes="i386-mingw32 i486-mingw32 i586-mingw32 i686-mingw32 \
 	i586-mingw32msvc i686-w64-mingw32 amd64-mingw32 \
-	x86_64-w64-mingw32 amd64-mingw32msvc"
+	x86_64-w64-mingw32 amd64-mingw32msvc mingw32"
 	
 if [ "X$make" = "X" ]; then
 	make="make"
@@ -18,14 +19,25 @@ fi
 
 function determine(){
 	for try in $prefixes; do
-		fulltry="${try}-gcc"
-		if [ ! $(which "$fulltry" 2>/dev/null) = "" ]; then
+		if [ ! $(which "${try}-gcc" 2>/dev/null) = "" ]; then
 			chain=$try
+		fi
+		# known path on Debian
+		if [ -d /usr/$try ]; then
+			prefix=/usr/$try
+		fi
+		# OpenBSD
+		if [ -d /usr/local/$try ]; then
+			prefix=/usr/local/$try
 		fi
 	done
 	
 	if [ "X$chain" = "X" ]; then
 		echo Do you even have mingw installed?
+		exit 1
+	fi
+	if [ "X$prefix" = "X" ]; then
+		echo Could not find mingw directory.
 		exit 1
 	fi
 }
@@ -36,15 +48,15 @@ function mkzlib(){
 	(cd zlib-1.2.7
 	./configure --static
 	$make --silent -j$procs -fwin32/Makefile.gcc PREFIX=$chain- RC=$chain-windres \
-		BINARY_PATH=/usr/$chain/bin INCLUDE_PATH=/usr/$chain/include \
-		LIBRARY_PATH=/usr/$chain/lib install)
+		BINARY_PATH=$prefix/bin INCLUDE_PATH=$prefix/include \
+		LIBRARY_PATH=$prefix/lib install)
 }
 
 function mklibogg(){
 	echo && echo mklibogg
 	curl -# http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz | gunzip | tar xf -
 	(cd libogg-1.3.0
-	./configure --prefix=/usr/$chain --host=$chain --target=$chain --enable-static \
+	./configure --prefix=$prefix --host=$chain --target=$chain --enable-static \
 		--disable-shared >/dev/null
 	$make --silent -j$procs && $make --silent install)
 }
@@ -53,7 +65,7 @@ function mklibvorbis(){
 	echo && echo mklibvorbis
 	curl -# http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz | gunzip | tar xf -
 	(cd libvorbis-1.3.3
-	./configure --prefix=/usr/$chain --host=$chain --target=$chain --enable-static \
+	./configure --prefix=$prefix --host=$chain --target=$chain --enable-static \
 		--disable-shared >/dev/null
 	$make --silent -j$procs
 	$make --silent install)
@@ -63,7 +75,7 @@ function mklibfreetype(){
 	echo && echo mklibfreetype
 	curl -# ftp://ftp.igh.cnrs.fr/pub/nongnu/freetype/freetype-2.4.10.tar.bz2 | bunzip2 | tar xf -
 	(cd freetype-2.4.10
-	./configure --prefix=/usr/$chain --host=$chain --target=$chain --enable-static \
+	./configure --prefix=$prefix --host=$chain --target=$chain --enable-static \
 		--disable-shared >/dev/null
 	$make -j$procs
 	$make --silent install &&
@@ -74,7 +86,7 @@ function mklibsdl(){
 	echo && echo mklibsdl
 	curl -# http://www.libsdl.org/release/SDL-1.2.15.tar.gz | gunzip | tar xf -
 	(cd SDL-1.2.15
-	./configure --prefix=/usr/$chain --host=$chain --target=$chain --enable-static \
+	./configure --prefix=$prefix --host=$chain --target=$chain --enable-static \
 		--disable-shared --enable-render-d3d=no >/dev/null
 	$make --silent -j$procs
 	$make --silent install)
@@ -84,7 +96,7 @@ function mklibjpeg(){
 	echo && echo mklibjpeg
 	curl -# http://ijg.org/files/jpegsrc.v8d.tar.gz | gunzip | tar xf -
 	(cd jpeg-8d
-	./configure --prefix=/usr/$chain --host=$chain --target=$chain --enable-static \
+	./configure --prefix=$prefix --host=$chain --target=$chain --enable-static \
 		--disable-shared >/dev/null
 	$make --silent -j$procs
 	$make --silent install)
@@ -94,7 +106,7 @@ function mklibcurl(){
 	echo && echo mklibcurl
 	curl -# http://curl.haxx.se/download/curl-7.28.1.tar.bz2 | bunzip2 | tar xf -
 	(cd curl-7.28.1
-	./configure --prefix=/usr/$chain --host=$chain --target=$chain --enable-static \
+	./configure --prefix=$prefix --host=$chain --target=$chain --enable-static \
 		--disable-shared --enable-ldap=no --enable-ldaps=no >/dev/null
 	$make --silent -j$procs
 	$make --silent install)
@@ -104,9 +116,9 @@ mkdir getlibs
 
 (cd getlibs
 determine &&
-rm -rf /usr/$chain/lib/libvorbis* /usr/$chain/lib/libogg* /usr/$chain/lib/libfreetype* \
-	/usr/$chain/lib/libz.* /usr/$chain/lib/libjpeg.* /usr/$chain/lib/libcurl.* \
-	/usr/$chain/lib/libSDL* /usr/$chain/include/SDL &&
+rm -rf $prefix/lib/libvorbis* $prefix/lib/libogg* $prefix/lib/libfreetype* \
+	$prefix/lib/libz.* $prefix/lib/libjpeg.* $prefix/lib/libcurl.* \
+	$prefix/lib/libSDL* $prefix/include/SDL &&
 mkzlib &&
 mklibogg &&
 mklibvorbis &&
