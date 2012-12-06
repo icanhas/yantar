f1bf58b08fc3a5e3c235e4a67ecc1969cecacb71
ich <icanhas@users.sf.net>
2012-12-06 17:48:56 +0000

    getlibs: definitely don't use non-portable --silent flag

--- getlibs.sh
+++ getlibs.sh
@@ -40,7 +40,7 @@ curl -# http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz | gunzip | ta
 (cd libogg-1.3.0
 ./configure --prefix=$prefix $cross --enable-static \
 	--disable-shared >/dev/null
-$make -s -j$procs && $make --silent install) &&
+$make -s -j$procs && $make -s install) &&
 
 echo && echo mklibvorbis
 curl -# http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz | gunzip | tar xf -
