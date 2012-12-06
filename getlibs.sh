6539cdd3ad95a9c9615cde5fbdd4b4d83e579c84
ich <icanhas@users.sf.net>
2012-12-06 17:45:28 +0000

    getlibs: don't use non-portable --silent flag

--- getlibs.sh
+++ getlibs.sh
@@ -31,7 +31,7 @@ echo && echo mkzlib
 curl -# http://zlib.net/zlib-1.2.7.tar.gz | gunzip | tar xf -
 (cd zlib-1.2.7
 ./configure --static
-$make --silent -j$procs $zcross \
+$make -s -j$procs $zcross \
 	BINARY_PATH=$prefix/bin INCLUDE_PATH=$prefix/include \
 	LIBRARY_PATH=$prefix/lib install) &&
 
@@ -40,15 +40,15 @@ curl -# http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz | gunzip | ta
 (cd libogg-1.3.0
 ./configure --prefix=$prefix $cross --enable-static \
 	--disable-shared >/dev/null
-$make --silent -j$procs && $make --silent install) &&
+$make -s -j$procs && $make --silent install) &&
 
 echo && echo mklibvorbis
 curl -# http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz | gunzip | tar xf -
 (cd libvorbis-1.3.3
 ./configure --prefix=$prefix $cross --enable-static \
 	--disable-shared >/dev/null
-$make --silent -j$procs
-$make --silent install) &&
+$make -s -j$procs
+$make -s install) &&
 
 echo && echo mklibfreetype
 curl -# ftp://ftp.igh.cnrs.fr/pub/nongnu/freetype/freetype-2.4.10.tar.bz2 | bunzip2 | tar xf -
@@ -56,7 +56,7 @@ curl -# ftp://ftp.igh.cnrs.fr/pub/nongnu/freetype/freetype-2.4.10.tar.bz2 | bunz
 ./configure --prefix=$prefix $cross --enable-static \
 	--disable-shared >/dev/null
 $make -j$procs
-$make --silent install &&
+$make -s install &&
 ln -sf /usr/$chain/include/freetype2/freetype /usr/$chain/include/freetype) &&
 
 echo && echo mklibsdl
@@ -64,24 +64,24 @@ curl -# http://www.libsdl.org/release/SDL-1.2.15.tar.gz | gunzip | tar xf -
 (cd SDL-1.2.15
 ./configure --prefix=$prefix $cross --enable-static \
 	--disable-shared --enable-render-d3d=no >/dev/null
-$make --silent -j$procs
-$make --silent install) &&
+$make -s -j$procs
+$make -s install) &&
 
 echo && echo mklibjpeg
 curl -# http://ijg.org/files/jpegsrc.v8d.tar.gz | gunzip | tar xf -
 (cd jpeg-8d
 ./configure --prefix=$prefix $cross --enable-static \
 	--disable-shared >/dev/null
-$make --silent -j$procs
-$make --silent install) &&
+$make -s -j$procs
+$make -s install) &&
 
 echo && echo mklibcurl
 curl -# http://curl.haxx.se/download/curl-7.28.1.tar.bz2 | bunzip2 | tar xf -
 (cd curl-7.28.1
 ./configure --prefix=$prefix $cross --enable-static \
 	--disable-shared --enable-ldap=no --enable-ldaps=no >/dev/null
-$make --silent -j$procs
-$make --silent install)
+$make -s -j$procs
+$make -s install)
 
 ) && echo && echo done && echo
 
