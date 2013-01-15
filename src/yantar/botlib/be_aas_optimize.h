490c519c9797092ea5f648d8dec12d360f41a9a6
ich <icanhas@users.sf.net>
2013-01-15 11:24:18 +0000

    botlib: clean aasoptimize.h

--- src/yantar/botlib/be_aas_optimize.h
+++ src/yantar/botlib/be_aas_optimize.h
@@ -5,13 +5,4 @@
  * it under the terms of the GNU General Public License.
  */
 
-/*****************************************************************************
-* name:		be_aas_optimize.h
-*
-* desc:		AAS
-*
-* $Archive: /source/code/botlib/be_aas_optimize.h $
-*
-*****************************************************************************/
-
-void AAS_Optimize(void);
+void	AAS_Optimize(void);
