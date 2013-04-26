62347a4b44b97a13f3812c27ebb58d35182e87f0
ich <icanhas@users.sf.net>
2013-04-26 22:40:58 +0100

    pm: revert to experimental accel function, this time with air control >85deg

--- src/yantar/bg/pmove.c
+++ src/yantar/bg/pmove.c
@@ -16,9 +16,7 @@
  */
 uint cnt = 0;
 
-static const Scalar GrapplePullSpeed	= 400.0f;
-static const Scalar Hooklinelen		= 2.0f;
-static const Scalar Maxhookforce	= 6.0f;
+static const Scalar Hookpullspeed	= 400.0f;
 static const Scalar pm_stopspeed	= 100.0f;
 static const Scalar pm_duckScale	= 0.25f;
 static const Scalar pm_swimScale	= 0.50f;
@@ -140,7 +138,8 @@ q2accelerate(Pmove *pm, Pml *pml, Vec3 wishdir, float wishspeed, float accel)
 	float addspeed, accelspeed, currentspeed;
 
 	currentspeed = dotv3(pm->ps->velocity, wishdir);
-	addspeed = wishspeed;
+	/* addspeed = wishspeed - currentspeed */
+	addspeed = wishspeed;	/* unlimited */
 	if(addspeed <= 0)
 		return;
 	accelspeed = accel*pml->frametime*wishspeed;
@@ -152,19 +151,29 @@ q2accelerate(Pmove *pm, Pml *pml, Vec3 wishdir, float wishspeed, float accel)
 static void
 accelerate(Pmove *pm, Pml *pml, Vec3 wishdir, float wishspeed, float accel)
 {
-	/* proper way (avoids strafe jump maxspeed bug), but feels bad */
-	Vec3	wishVelocity;
-	Vec3	pushDir;
-	float	pushLen;
-	float	canPush;
+	int i;
+	float addspeed, accelspeed, speed, div, d;
+	Vec3 vel;
 
-	scalev3(wishdir, wishspeed, wishVelocity);
-	subv3(wishVelocity, pm->ps->velocity, pushDir);
-	pushLen = normv3(pushDir);
-	canPush = accel*pml->frametime*wishspeed;
-	if(canPush > pushLen)
-		canPush = pushLen;
-	saddv3(pm->ps->velocity, canPush, pushDir, pm->ps->velocity);
+	speed = dotv3(pm->ps->velocity, wishdir);
+	addspeed = wishspeed - 0.25f*speed;
+	if(addspeed <= 0)
+		return;
+	accelspeed = accel*pml->frametime*wishspeed;
+	if(accelspeed > addspeed)
+		accelspeed = addspeed;
+	saddv3(pm->ps->velocity, accelspeed, wishdir, vel);
+	
+	speed = normv3(vel);
+	div = dotv3(vel, wishdir);
+	d = 32;
+	d *= 150.0f * div * div * pml->frametime;
+	if(RAD2DEG(acos(div) > 85.0f)){	/* same as div < 0.08715574692f */
+		scalev3(vel, speed, vel);
+		saddv3(vel, d, wishdir, vel);
+		normv3(vel);
+	}
+	scalev3(vel, speed, pm->ps->velocity);
 }
 
 /*
@@ -455,7 +464,7 @@ airmove(Pmove *pm, Pml *pml)
 	dofriction(pm, pml);
 	_airmove(pm, pml, &pm->cmd, &wishvel, &wishdir, &wishspeed);
 	/* not on ground, so little effect on velocity */
-	q2accelerate(pm, pml, wishdir, wishspeed, pm_airaccelerate);
+	accelerate(pm, pml, wishdir, wishspeed, pm_airaccelerate);
 	PM_StepSlideMove(pm, pml, qtrue);
 }
 
@@ -463,7 +472,7 @@ static void
 grapplemove(Pmove *pm, Pml *pml)
 {
 	Vec3 wishvel, wishdir, vel, v;
-	float	wishspeed, vlen, oldlen, pullspeedcoef, grspd = GrapplePullSpeed;
+	float	wishspeed, vlen, oldlen, pullspeedcoef, grspd = Hookpullspeed;
 
 	_airmove(pm, pml, &pm->cmd, &wishvel, &wishdir, &wishspeed);
 	scalev3(pml->forward, -16, v);
@@ -479,8 +488,8 @@ grapplemove(Pmove *pm, Pml *pml)
 		pullspeedcoef *= pm->ps->swingstrength;
 		grspd *= pullspeedcoef;
 	}
-	if(grspd < GrapplePullSpeed)
-		grspd = GrapplePullSpeed;
+	if(grspd < Hookpullspeed)
+		grspd = Hookpullspeed;
 	
 	normv3(vel);
 	q2accelerate(pm, pml, wishdir, wishspeed, pm_airaccelerate);
